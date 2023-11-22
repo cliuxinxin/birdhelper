b74acdc91e5f5ec73beb28509e79e25395a0f61d
liuxinxin <cliuxinxin@gmail.com>
2023-11-22 16:26:25 +0800

    更改部署代码

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,7 @@
 
 cd /root/birdhelper
 
-# 设置最大尝试次数和重试延时
-MAX_RETRIES=3
-RETRY_DELAY=2
-
-# 执行git pull，直到成功或达到最大尝试次数
-attempt=1
-while [ $attempt -le $MAX_RETRIES ]
-do
-    git pull && break
-    echo "Attempt $attempt failed! Trying again in $RETRY_DELAY seconds..."
-    attempt=$((attempt+1))
-    sleep $RETRY_DELAY
-done
-
-if [ $attempt -gt $MAX_RETRIES ]; then
-    echo "Failed to git pull after $MAX_RETRIES attempts."
-    exit 1
-fi
+git pull
 
 # 继续执行后续命令
 docker build -t slack-bot . --no-cache
