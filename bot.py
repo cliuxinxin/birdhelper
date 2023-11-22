69a6ee6fcaf810e4c2e477ca5a5827bd29108740
liuxinxin <cliuxinxin@gmail.com>
2023-11-22 17:11:26 +0800

    测试

--- bot.py
+++ bot.py
@@ -11,34 +11,31 @@ client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
 @app.route('/slack/events', methods=['POST'])
 def slack_events():
-    try:
-        print("Received a request")
-        print(request.get_data(as_text=True))
-        data = request.json
+    print("Received a request")
+    print(request.get_data(as_text=True))
+    data = request.json
 
-        # 方便调试
-        print(data)
+    # 方便调试
+    print(data)
 
-        # 响应Slack的URL验证
-        if 'challenge' in data:
-            return jsonify({'challenge': data['challenge']})
+    # 响应Slack的URL验证
+    if 'challenge' in data:
+        return jsonify({'challenge': data['challenge']})
 
-        # 检查是否存在 'event' 键
-        if 'event' in data:
-            event = data['event']
+    # 检查是否存在 'event' 键
+    if 'event' in data:
+        event = data['event']
 
-            # 处理app_mention事件
-            if event.get('type') == 'app_mention':
-                try:
-                    response = client.chat_postMessage(
-                        channel=event['channel'],
-                        text="Hello, I am your bot!")
-                except SlackApiError as e:
-                    print(f"Error posting message: {e}")
+        # 处理app_mention事件
+        if event.get('type') == 'app_mention':
+            try:
+                response = client.chat_postMessage(
+                    channel=event['channel'],
+                    text="Hello, I am your bot!")
+            except SlackApiError as e:
+                print(f"Error posting message: {e}")
 
-        return '', 200
-    except Exception as e:
-        print(f"Exception occurred: {e}")
+    return '你好', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
