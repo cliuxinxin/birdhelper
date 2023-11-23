d9702d62194ae7f7d636c4433f51cdce3fef7960
liuxinxin <cliuxinxin@gmail.com>
2023-11-23 10:26:18 +0800

    部署代码更改

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cd /root/birdhelper
 git pull
 
 # 继续执行后续命令
-docker build -t slack-bot .
-docker stop slack-bot || true
-docker rm slack-bot || true
-docker run -d --name slack-bot -p 5000:5000 slack-bot
+docker build -t tele-bot .
+docker stop tele-bot || true
+docker rm tele-bot || true
+docker run -d --name tele-bot -p 5000:5000 tele-bot
